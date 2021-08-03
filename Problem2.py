a22aa9674326150374c865771b40a24c84445c9c
C0MPU73R <55300593+C0MPU73R@users.noreply.github.com>
2021-08-03 02:49:42 -0400

    Create Problem2.py

---
+++ Problem2.py
@@ -0,0 +1,16 @@
+"""
+Filename: Problem2.py
+
+@ author: C0MPU73R
+"""
+
+
+# Generate and store fibonacci numbers
+# The general algorithm is:
+# 0, 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, etc
+# This can be written as: Xn+2 = Xn+1 + Xn
+# In terms of a function: Fn = Fn-1 + Fn-2
+# the current term is equivalent to the sum of the last two terms
+
+def fibonacci():
+    seq = []
